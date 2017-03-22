4f0c21da6d18ed09b5e4b42cd4186c5247e515c3
Pulkit219 <ascension1110@gmail.com>
2017-03-22 17:56:42 -0400

    3rd comm

--- vueapp/src/App.vue
+++ vueapp/src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <test></test>
    
   </div>
 </template>
 
 <script>
-
+import Test from './components/Test'
 
 export default {
   name: 'app',
   components: {
-   
+   Test
   }
 }
 </script>
---
+++ vueapp/src/components/Test.vue
@@ -0,0 +1,39 @@
+<template> 
+    <div class="test">
+        <h1>{{title}}</h1>
+        <p>{{user.first_name}}</p>
+
+    </div>
+
+</template>
+
+
+<script>
+   export default {
+     name:'test',
+     data(){
+         return{
+
+
+
+             title:'hello world ',
+        user:{
+            first_name:'pulkit',
+            last_name:'bansal'
+            }
+         }
+
+        
+
+     }
+
+   }
+
+</script>
+
+
+
+
+<stytle scoped>
+
+</style>
