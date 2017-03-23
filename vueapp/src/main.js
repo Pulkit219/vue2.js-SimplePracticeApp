ec4207704aee2b3e92bbb63f6a6accd5a08fa4cf
Pulkit219 <ascension1110@gmail.com>
2017-03-22 23:31:10 -0400

    10th comm

--- vueapp/src/main.js
+++ vueapp/src/main.js
@@ -3,14 +3,43 @@
 import Vue from 'vue'
 import App from './App'
 import vueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+import Users from './components/users'
+import Test from './components/test'
 
 Vue.use(vueResource)
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+   mode: 'history',
+   base: __dirname,
+   routes:[
+     {path:'/',component:Users},
+     {path: '/test',component:Test}
+   ]
+
+}
+ 
+
+);
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
-})
+  // el: '#app',
+  router,
+  template: `
+  <div id="app">
+
+  <ul>
+     <li><router-link to="/">Users</router-link></li>
+     <li><router-link to="/test">Test</router-link></li>
+  </ul>
+  <router-view></router-view>
+  </div>
+  
+  `,
+  // components: { App }
+}).$mount('#app')
